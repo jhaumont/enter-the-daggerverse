9f090b93003b995449e0cc15a2f36fa9a0c874c2
Emeric MARTINEAU <11473190+emeric-martineau@users.noreply.github.com>
2025-04-13 16:14:01 +0000

    Add git aliases

--- .devcontainer/install-dagger.sh
+++ .devcontainer/install-dagger.sh
@@ -9,4 +9,13 @@ echo "autoload -U compinit" >> $HOME/.zshrc
 echo "compinit -i" >> $HOME/.zshrc
 echo "sudo chmod a+rwx /var/run/docker.sock" >> $HOME/.zshrc
 $HOME/.local/bin/dagger completion zsh > /usr/share/zsh/site-functions/_dagger
+echo "[alias]" >> $HOME/.gitconfig
+echo "co = checkout" >> $HOME/.gitconfig
+echo "br = branch" >> $HOME/.gitconfig
+echo "ci = commit" >> $HOME/.gitconfig
+echo "st = status" >> $HOME/.gitconfig
+echo 'l = log --graph --pretty=format:\"%C(yellow)%h%Creset%C(red)%d%Creset %C(cyan)%cr%Creset %s %Creset%C(green)%an\"' >> $HOME/.gitconfig
+echo "lf = log --pretty=fuller" >> $HOME/.gitconfig
+echo "unadd = reset HEAD^" >> $HOME/.gitconfig
+echo "graph = log --graph --abbrev-commit --decorate --format=format:'%C(yellow)%h%C(reset)%C(auto)%d%C(reset) %C(normal)%s%C(reset) %C(dim magenta)%an%C(reset) %C(dim blue)(%ar) %C(reset)' --all" >> $HOME/.gitconfig
 EOF
