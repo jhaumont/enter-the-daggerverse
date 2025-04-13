749915668b38d4b50cfec654a4a39b35131ec9be
Emeric MARTINEAU <11473190+emeric-martineau@users.noreply.github.com>
2025-04-13 16:09:27 +0000

    Fix issue with Docker socket after CodeSpace restart

--- .devcontainer/install-dagger.sh
+++ .devcontainer/install-dagger.sh
@@ -3,10 +3,10 @@
 mkdir -p "$HOME/.local/bin"
 curl -fsSL https://dl.dagger.io/dagger/install.sh | DAGGER_VERSION=0.18.2 BIN_DIR=$HOME/.local/bin sh
 sudo su <<EOF
-chmod 777 /var/run/docker.sock
 echo "alias ll='ls -al'" >> $HOME/.zshrc
 echo "PATH=$PATH:$HOME/.local/bin" >> $HOME/.zshrc
 echo "autoload -U compinit" >> $HOME/.zshrc
-echo "compinit" -i >> $HOME/.zshrc
+echo "compinit -i" >> $HOME/.zshrc
+echo "sudo chmod a+rwx /var/run/docker.sock" >> $HOME/.zshrc
 $HOME/.local/bin/dagger completion zsh > /usr/share/zsh/site-functions/_dagger
 EOF
